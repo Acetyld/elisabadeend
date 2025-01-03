b13f8e2e66a7167e92c63746b61278f2de17cbb5
Dion <dion@vitrion.nl>
2025-01-03 20:22:40 +0100

    Update asset references in index.html, remove unused CSS and JS files, and ensure Tailwind CSS is imported correctly in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './index.css';  // Importeer Tailwind CSS
 // src/main.ts
 import { createApp } from 'vue';  // Importeer de Vue bibliotheek
 import App from './App.vue';  // Importeer de hoofdcomponent van je app
 import router from './router';  // Importeer de router die we in stap 1 hebben gemaakt
+import './index.css';  // Importeer Tailwind CSS
 
 // Maak de applicatie aan en gebruik de router
 const app = createApp(App);
